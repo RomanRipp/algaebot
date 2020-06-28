3d7c29242a85215daf23983e2263d207a4dc4f26
Roman Ripp <rippx009@gmail.com>
2020-06-28 13:00:03 -0700

    Light sensor plug in

--- src/sensors/light.go
+++ src/sensors/light.go
@@ -1,9 +1,26 @@
 package sensors
 
-type Light struct {
+import (
+	"github.com/stianeikeland/go-rpio"
+)
 
+type Light struct {
+	pinNumber int
+	lastReading float64
 }
 
 func (s Light) Read() (float64, error) {
-	return 0.0, nil
+	err := rpio.Open()
+	if  err == nil {
+		pin := rpio.Pin(s.pinNumber)
+		pin.Input()
+		switch pin.Read() {
+		case 0:
+			s.lastReading = 0.0
+		case 1:
+			s.lastReading = 1.0
+		}
+	}
+	defer rpio.Close()
+	return s.lastReading, err
 }
