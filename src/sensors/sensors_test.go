0bf381eaba8ec215fd4a4f40ee3745208e977d05
Roman Ripp <rippx009@gmail.com>
2020-06-29 18:37:06 -0700

    Test logging clean up

--- src/sensors/sensors_test.go
+++ src/sensors/sensors_test.go
@@ -8,16 +8,19 @@ func TestLightSensor(t *testing.T) {
 	light := Light{}
 	light.pinNumber = 23
 	value, error := light.Read()
-	if value !=  0.0 || error != nil {
+	if error != nil {
 		t.Error("Invalid sensor reading")
+	} else {
+		t.Log("Photo sensor reading: ", value)
 	}
+
 }
 
 func TestSonarSensor(t *testing.T) {
 	sonar := Sonar{triggerPinNum: 16, echoPinNum: 6}
 	distance, error := sonar.ReadDistance()
 	if error == nil {
-		t.Log(distance)
+		t.Log("Distance sensor reading: ", distance)
 	} else {
 		t.Error("Failed to read sensor.")
 	}
